7f5d5dd1a998ed05a43943a26abbbb529dd4d451
Loucleme03 <147090760+Loucleme03@users.noreply.github.com>
2023-10-20 17:26:08 -0400

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -185,10 +185,10 @@ function showSlides(n) {
 // Demo 4 - esri-Leaflet Map    
 //Create Map set View
      const apiKey = "AAPK12b8f9e1da2940bbbe0febf69977fe0eRsGXL4uHQ8jvzClzGSzqfQRamq1XnSFNukmkNN0wrlrtSJriluYaeTPmVwwEpgCS";
-      const basemapEnum = "ArcGIS:LightGray:Base";
+      const basemapEnum = "ArcGIS:LightGray";
       const map2 = L.map("map2", {
         minZoom: 2
-      }).setView([44.6308, -79.0548], 12); 
+      }).setView([44.6308, -79.0548], 10); 
      L.esri.Vector.vectorBasemapLayer(basemapEnum, {
         apiKey: apiKey
       }).addTo(map2);
